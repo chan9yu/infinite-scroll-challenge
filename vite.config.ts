40dd59f6455f9df702ef403ec7d53b0519100cb2
chan9yu <dev.cgyeo@gmail.com>
2024-10-05 21:42:34 +0900

    chore: github page deploy 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    open: true,
-    port: 3035,
-  },
+	base: '/infinite-scroll-challenge/',
+	plugins: [react()],
+	server: {
+		open: true,
+		port: 3035
+	}
 });
